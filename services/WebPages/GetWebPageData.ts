2c3c6dc62b11d2361471968f938966a934105012
Nick <nick.stepanenko@trilogy.com>
2022-11-24 18:22:49 +0300

    automated commit

--- services/WebPages/GetWebPageData.ts
+++ services/WebPages/GetWebPageData.ts
@@ -1,8 +1,14 @@
 import { APIGatewayEvent, APIGatewayProxyResult, Context } from 'aws-lambda';
 import ReturnRestApiResult from 'services/Utils/ReturnRestApiResult';
-import { TelegramUserFromAuthorizer } from 'services/Utils/Types';
-//@ts-ignore
-import BotManager from '/opt/BotManager';
+import { ddbDocClient } from '/opt/DDB/ddbDocClient';
+import { defaultMenuLanguage, ESupportedLanguages } from '/opt/ConfiguratorTypes';
+
+type Page = {
+    pagePath: string;
+    locale: ESupportedLanguages;
+    pageId?: string;
+};
+const fallbackLocale = defaultMenuLanguage;
 
 export async function GetWebPageDataHandler(event: APIGatewayEvent, context: Context): Promise<APIGatewayProxyResult> {
     console.log(event);
@@ -15,15 +21,45 @@ export async function GetWebPageDataHandler(event: APIGatewayEvent, context: Con
             origin = event.headers.origin;
         }
     }
+    if (!event.body) {
+        console.log('body not provided');
+        const returnObject = ReturnRestApiResult(422, { error: 'body not provided' }, origin);
+        return returnObject as APIGatewayProxyResult;
+    }
+    try {
+        const body = JSON.parse(event.body);
+        if (!body.pagePath) {
+            const returnObject = ReturnRestApiResult(422, { error: 'path not provided' }, origin);
+            return returnObject as APIGatewayProxyResult;
+        }
+        let locale = !body.locale ? fallbackLocale : body.locale;
+        try {
+            const dbResponce = await ddbDocClient.query({
+                TableName: process.env.webTable!,
+                KeyConditionExpression: 'PK = :PK, SK = :SK',
+                ExpressionAttributeValues: {
+                    ':PK': 'LOCALE#' + locale + '#PATH#' + body.pagePath
+                }
+            });
+            const map = new Map<string, any>();
+            if (dbResponce.Items) {
+                for (const item of dbResponce.Items) {
+                    map.set(item.SK, item.data);
+                }
+            }
+            const returnObject = ReturnRestApiResult(200, JSON.stringify(map), origin);
+            return returnObject as APIGatewayProxyResult;
+        } catch (error) {
+            console.log('DynamoDB error\n', error);
+            const returnObject = ReturnRestApiResult(422, { error: 'Database error' }, origin);
+            return returnObject as APIGatewayProxyResult;
+        }
+    } catch (error) {
+        console.log('error: Body is not in JSON');
+        const returnObject = ReturnRestApiResult(422, { error: 'Body is not in JSON' }, origin);
+        return returnObject as APIGatewayProxyResult;
+    }
 
-    const telegramUser = event.requestContext.authorizer as TelegramUserFromAuthorizer;
-
-    const botManager = await BotManager.GetOrCreate({
-        chatId: telegramUser.id,
-        userName: telegramUser.username
-    });
-
-    botManager.GetMyBots();
     const returnObject = ReturnRestApiResult(200, { templateData: 'template' }, origin);
 
     console.log('returnObject\n', returnObject);
