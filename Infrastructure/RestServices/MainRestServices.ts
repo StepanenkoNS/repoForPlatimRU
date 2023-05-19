2c7057e0a0ea162f57315e8db06ec07ce4e1e235
Nick <nick.stepanenko@trilogy.com>
2023-05-19 14:30:33 +0300

    automated commit

--- Infrastructure/RestServices/MainRestServices.ts
+++ Infrastructure/RestServices/MainRestServices.ts
@@ -64,7 +64,7 @@ export class MainRestServicesStack extends Stack {
         const meetingLambdas = CreateCalendarMeetingsLambdas(this, layers, [botsTable]);
 
         this.lambdaIntegrations.push({
-            rootResource: 'Meetings',
+            rootResource: 'CalendarMeeting',
             lambdas: meetingLambdas
         });
 
