7edaead1440411e82edb769abc895706e00d65da
Nick <nick.stepanenko@trilogy.com>
2023-05-24 11:34:47 +0300

    automated commit

--- test/PaymentProcessor/testConfirmPayment.ts
+++ test/PaymentProcessor/testConfirmPayment.ts
@@ -3,21 +3,22 @@ import { handler } from 'services/PaymentProcessor/IncomingPaymentConfirmation';
 const event = {
     Records: [
         {
-            messageId: 'ecc498ac-8f6c-4996-83fa-fb380454b965',
+            messageId: '741bd735-f636-4e6e-b336-57eaf8d37dc2',
             receiptHandle:
-                'AQEB1Yz8ZzNDa6Yg7LitJBBfRvoSF9F65v/5gl53kXNHKGVTg5H4wdn2LJ6ZRzUmxI2mglV9HY6QpfBEDBUBJKkTmzOYF0OYEc/n6xkCaHnBtlG1YUIXrvOCxTV0bXsBHy6JDAQJeSLBrvnns0E8ewXo4lfDWNXMhhVjTsFiXRcVwS8TQIL6BQEeonwOiJnGTbl31hQ+3cDJGeEVGRF8G3W9hJn7rdfYY+Tonckx87v4SJ60w+0s3jQK54fyXHOYjyC31NQF53K8F+xPaJ+pvK13HfrtWjpIWLNCf+f0gRdAj2KMo5wxTzgaXvZ4DFBR4ELg',
-            body: '{"discriminator":"IRequestToConfirmPayment","botId":5795087844,"masterId":199163834,"id":"2QBuMSEpxCDhIHuq9Z2xiZsUXVZ","action":"Confirm"}',
+                'AQEBfh/MlqZOqLGIC0E7EqImVnsO83xk6KwsRgoyuKi+6JPHq1jG8q/+PtHJuA+zBgu/8JOoYJsvvgLRu4xriuz84uATkc+nJRzrDJNu2KyLfAmcavzLy9avy/NilwL7/88FZPYxY4Kk1fhPddQR6uSbDLRT5dxVa2Bt3bhNhZPlVffqyABSvcj/BuPIYptjTJexaDGyFFcMhB2Yd0cIMpfC0vNkeylU0HopTo0/KP5TYGBpcFYgUizaP54UOJTn4wiWvRCdDgqLyT3mIMZkq3TteuXHoyRhCMNOjUa3kfRcUuoz7jp6COzEG8DjqA0McRRE',
+            body: '{"chatId":1862254,"botId":5795087844,"masterId":199163834,"paymentTarget":"SUBSCRIPTION","subscriptionPlanId":"roD9e3Th553S","channelId":-1001874319435,"subscriptionPlanName":"Короткая","subscriptionType":"CHANNEL","price":"100","currency":"RUB","paymentOptionId":"4ej8dY9rrvZx","paymentOptionType":"DIRECT","telegramMessageText":"","telegramSendMethod":"sendPhoto","telegramFileId":"AgACAgIAAxkBAAIaSWRtkeCoVI5XmEvJi1wqzxEWpCV9AAJMxTEbj7xwS5XoxQ0YvgghAQADAgADeQADLwQ"}',
             attributes: {
                 ApproximateReceiveCount: '1',
-                SentTimestamp: '1684839277776',
-                SequenceNumber: '18878062928820210688',
+                AWSTraceHeader: 'Root=1-646d91e0-10fd91ea54cc99602df1cf62;Parent=4850ba3b5e890fd1;Sampled=0;Lineage=7acea675:0|47d82b4f:0',
+                SentTimestamp: '1684902368938',
+                SequenceNumber: '18878079080157679616',
                 MessageGroupId: '5795087844',
-                SenderId: 'AIDA6OX3PF47UX6FRE7SI',
-                MessageDeduplicationId: '2QBuQELiob6g13yh0zUgxyIy4D6',
-                ApproximateFirstReceiveTimestamp: '1684839277776'
+                SenderId: 'AROA6OX3PF47TZJE6YIGG:Bot-messagingBot-Lambda',
+                MessageDeduplicationId: '2QDyImMqBFzblnVFeom6gxgCDhO',
+                ApproximateFirstReceiveTimestamp: '1684902368938'
             },
             messageAttributes: {},
-            md5OfBody: '828a2ad9b08dd5cff5c67a2e7f98400e',
+            md5OfBody: '06e13ae323bbf924ae49020890f7c3c8',
             eventSource: 'aws:sqs',
             eventSourceARN: 'arn:aws:sqs:us-east-1:993738567487:paymentProcessor-ConfirmationRequest.fifo',
             awsRegion: 'us-east-1'
