710a7ec4c0a05b7a9fe3a234d4d3080ba862d7af
Nick <nick.stepanenko@trilogy.com>
2023-07-08 21:03:49 +0300

    automated commit

--- Infrastructure/Launcher.ts
+++ Infrastructure/Launcher.ts
@@ -6,20 +6,27 @@ import { DynamicEnvironment } from '../../../Core/ReadmeAndConfig/DynamicEnviron
 
 import { PaymentIntegrationsStack } from './PaymentIntegrations/PaymentIntegrations';
 
-const app = new App();
+async function main() {
+    const app = new App();
 
-const environment = getConfig(app) as EEnvironment;
-if (!EEnvironment) {
-    throw 'Environment is not defined';
-}
-
-const paymentIntegrationsStack = new PaymentIntegrationsStack(app, StackName.PaymentIntegrationsAndGateWay.toString(), {
-    stackName: StackName.PaymentIntegrationsAndGateWay.toString(),
-    certificateARN: DynamicEnvironment(props.environment).Certificates.domainCertificateARN,
-
-    enableAPICache: false,
-    env: {
-        account: StaticEnvironment(environment).AwsSettings.account,
-        region: StaticEnvironment(environment).AwsSettings.region
+    const environment = getConfig(app) as EEnvironment;
+    if (!EEnvironment) {
+        throw 'Environment is not defined';
     }
-});
+    const certificateARN = await CertificateARN(environment, StaticEnvironment(environment).WebResources.mainDomainName);
+    if (!certificateARN) {
+        throw 'CertificateARN is undefined';
+    }
+
+    const paymentIntegrationsStack = new PaymentIntegrationsStack(app, StackName.PaymentIntegrationsAndGateWay.toString(), {
+        stackName: StackName.PaymentIntegrationsAndGateWay.toString(),
+        certificateARN: DynamicEnvironment(props.environment).Certificates.domainCertificateARN,
+
+        enableAPICache: false,
+        env: {
+            account: StaticEnvironment(environment).AwsSettings.account,
+            region: StaticEnvironment(environment).AwsSettings.region
+        }
+    });
+}
+main();
